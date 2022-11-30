d00df2a91c7b45ad34058b36959988414c713d50
Gustavo <gustavo_aedo@live.com>
2022-11-30 18:46:25 -0300

    Se agrega carrusel en home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,14 +2,45 @@ import banner from './../assets/banner.jpg'
 import Header from './Header'
 
 
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <div className="container-fluid">
             <div className='contenedor-home'>
                 <Header></Header>
                 <div className="row">
                     <div className='titulo-home'>
                         Bienvenidos a Ricascarnes.com
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="carousel-width">
+                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+                            <div className="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="https://static.diariovasco.com/www/multimedia/201906/03/media/cortadas/carne-roja-kS1C-R5uqUSGFlSqj84mn1I9bQuN-624x385@Diario%20Vasco.jpg" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="https://cocina-casera.com/wp-content/uploads/2015/05/carne-de-pollo-1.jpg" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="https://thefoodtech.com/wp-content/uploads/2020/09/maduracion-de-carne-de-pollo.jpg" className="d-block w-100" alt="..." />
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
